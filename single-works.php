b1ca35e19b75dec7383ba6b4aefb8aa2f8ecce1f
mizuirostudio <magmidori@gmail.com>
2024-04-18 13:50:36 +0900

    CSS一部修正しました

--- single-works.php
+++ single-works.php
@@ -5,12 +5,14 @@
   <?php if (have_posts()) : the_post(); ?>
     <div class="tag"><?php echo get_the_term_list($post->ID, 'works_tag', '<ul><li>', '</li><li>', '</li></ul>'); ?></div>
     <div class="the-time"><?php the_time('Y.m.d'); ?></div>
-    <h1><?php echo wp_get_document_title(); ?></h1>
-    <?php
-    echo get_the_post_thumbnail($post->ID, 'large');
-    ?>
+    <div class="page-contents">
+      <h1><?php echo wp_get_document_title(); ?></h1>
+      <?php
+      echo get_the_post_thumbnail($post->ID, 'large');
+      ?>
 
-    <?php the_content(); ?>
+      <?php the_content(); ?>
+    </div>
   <?php endif; ?>
 </article>
 <?php breadcrumb(); ?>
