ce764dcc967fb1aa8c7876e5ade020ab0dfb38a4
mizuirostudio <magmidori@gmail.com>
2024-04-19 15:26:03 +0900

    一部ページ修正しました

--- page.php
+++ page.php
@@ -2,14 +2,14 @@
 <?php breadcrumb(); ?>
 <article id="page-article" class="wrapper page-wrapper">
   <?php if (have_posts()) : the_post(); ?>
-    <h1><?php echo wp_get_document_title(); ?></h1>
+    <div class="page-contents">
+      <h1><?php echo wp_get_document_title(); ?></h1>
+      <?php
+      echo get_the_post_thumbnail($post->ID, 'large');
+      ?>
 
-    <?php
-    echo get_the_post_thumbnail($post->ID, 'large');
-    ?>
-    <div class="tag"><?php echo get_the_term_list($post->ID, 'works-tag', '<ul><li>', '</li><li>', '</li></ul>'); ?></div>
-
-    <?php the_content(); ?>
+      <?php the_content(); ?>
+    </div>
   <?php endif; ?>
 </article>
 <?php breadcrumb(); ?>
